ba2c984ac8fe63628d5e5f6ff5a035e063371b44
KiraboKarynaKiwagama <k.kiwagama@alustudent.com>
2026-02-27 03:42:49 -0800

    First fix

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-
+ language that combines remarkable power with very clear synta"
 print(f"{str[39:66]} with python")
--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("The Zen of python, by Tim Peters\n Beautiful is better than ugly.\n Explicit is better than implicit. \n Simple is better than complex. \n Complex is better than complicated. \n Flat is better than nested.")
+print("The Zen of python, by Tim Peters\n Beautiful is better than ugly.\n")
